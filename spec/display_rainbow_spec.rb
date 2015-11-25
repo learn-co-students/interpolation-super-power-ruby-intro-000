304b0149a24cf1a84ab4afd47fc7a35ac8d899c6
aviflombaum <avi@flombaum.com>
2015-11-25 14:55:57 -0500

    minor update to test

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -9,12 +9,14 @@ describe '#display_rainbow' do
   it 'prints out the colors of the rainbow correctly when passed in in order' do
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
+    expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
+    expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
+    expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
+    expect(colors).to receive(:[]).with(3).at_least(:once).and_return("green")
+    expect(colors).to receive(:[]).with(4).at_least(:once).and_return("blue")
+    expect(colors).to receive(:[]).with(5).at_least(:once).and_return("indigo")
+    expect(colors).to receive(:[]).with(6).at_least(:once).and_return("violet")
+
     expect { display_rainbow(colors) }.to output("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n").to_stdout
-  end
-
-  it 'prints out the colors of the rainbow in the order in which they are passed in' do
-    colors = ['orange', 'red', 'green', 'yellow', 'blue', 'violet', 'indigo']
-
-    expect { display_rainbow(colors) }.to output("R: orange, O: red, Y: green, G: yellow, B: blue, I: violet, V: indigo\n").to_stdout
   end
 end

da671007ce3060426cb9c870478676677cf7a09b
Cameron Dunne <camdunne@gmail.com>
2016-12-05 01:58:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(array)
+  final = ""
+  i = 0
+  while i < array.length
+    final += array[i][0].upcase + ": " + array[i]
+    if i != array.length-1
+      final += ", "
+    end
+    i += 1
+  end
+  puts final
+end

7f5e399a9bc89a6ee132ff989ec55f4335202799
kasia rosenberg <kasia@luckybidwell.com>
2016-12-14 23:23:03 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+require 'pry'
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(colors)
+
+  #binding.pry
+  #puts "R: #{colors[0]}, O: #{colors[1]}, Y:#{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, B: #{colors[5]}, I: #{colors[6]}, V: #{colors[7]};"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+display_rainbow(colors)

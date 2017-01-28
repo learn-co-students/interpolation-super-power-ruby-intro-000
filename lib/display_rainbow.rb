1d9552e5b47910506008b6fb6900b0d17044acb5
Eser SAHIN <eser.sahin@gmail.com>
2017-01-28 19:08:14 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+def display_rainbow(color_array)
+  letters = ["R","O","Y","G","B","I","V"]
+  puts "#{letters[0]}: #{color_array[0]}, #{letters[1]}: #{color_array[1]}, #{letters[2]}: #{color_array[2]}, #{letters[3]}: #{color_array[3]}, #{letters[4]}: #{color_array[4]}, #{letters[5]}: #{color_array[5]}, #{letters[6]}: #{color_array[6]}"
+end

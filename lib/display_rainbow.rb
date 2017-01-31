572716c35fb6dfabb82245694e8050e035950738
Adam Ergas <adamergas@yahoo.com>
2017-01-31 18:07:50 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  # colors.each do |color|
+  #   print "#{color[0].upcase}: #{color}"
+  #   if color != colors[colors.length-1]
+  #     print ", "
+  #   end
+  # end
+end

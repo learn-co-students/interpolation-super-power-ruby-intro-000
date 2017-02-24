2302ec8dd858fd26bdada4906cb32d39a7a2765d
Jelo Yambao <jelolearns@gmail.com>
2017-02-24 22:53:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,17 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  i = 0
+  i_int = i.to_i
+  colors.length.times do
+    color_initial = "#{colors[i_int].slice(0).upcase}"
+    color = "#{colors[i_int]}"
+    i = i+1
+    i_int = i.to_i
+    if i_int < colors.length
+      print color_initial + ": " + color + ", "
+    else
+      print color_initial + ": " + color + "\n"
+  end
+end
+end

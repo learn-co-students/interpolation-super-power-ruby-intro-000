ba76762e635626092b3ebb67b39be8963e11d476
James Thomas <jamesjacobthomas7@gmail.com>
2017-05-04 06:01:34 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow(james)
+  puts "R: #{james[0]}, O: #{james[1]}, Y: #{james[2]}, G: #{james[3]}, B: #{james[4]}, I: #{james[5]}, V: #{james[6]}"
+end

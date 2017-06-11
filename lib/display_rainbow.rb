d80004ca669c38dab74ca9b160ce403a92fa37ca
Zainab Sayeed <zainab.sayeed124@gmail.com>
2017-06-11 02:39:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+
+
+def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+print "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}\n"
+end
+
+
+
+# R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet => nil getting this
+# "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n" expecting this
+# only difference is the quotation marks.
+
+#pass the colors through the argument. hardcoare the order of array..

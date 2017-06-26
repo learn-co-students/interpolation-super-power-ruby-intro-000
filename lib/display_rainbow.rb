077a353e3dc5f143b5ff6f88627e42ba71020add
Di Livius <liviu.sirghea@yahoo.com>
2017-06-26 09:34:38 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
+  result = ""
+  colors.each do |color|
+    result += "#{color[0,1].upcase}: #{color.downcase}, "
+  end
+  puts result[0,result.length - 2]
+end
+
+#display_rainbow(["red", "yellow", "blue", "purple"])

1c7b1b583acbab0deb9e9d0d07d3df859620106b
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-02 18:20:07 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(colors)
+  color_string = []
+  n = colors.length
+  n.times do |i|
+    color_string.push("#{colors[i].chars.first.upcase}" + ": " + "#{colors[i].downcase}")
+  end
+
+  puts "#{color_string.join(", ")}"
+
+end

4ba3a572bb54657ee14fa71ec9e037eab69a9df9
Nana O <nancila.grace@gmail.com>
2018-02-04 17:31:01 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,35 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  #colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+#  colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+#    print "R: #{colors[0]}, "
+#    print "O: #{colors[1]}, "
+#    print "Y: #{colors[2]}, "
+#    print "G: #{colors[3]}, "
+#    print "B: #{colors[4]}, "
+#    print "I: #{colors[5]}, "
+#    print "V: #{colors[6]}"
+
+#  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
+
+#  colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+#  puts "R: #{colors[0]},"
+#  puts "O: #{colors[1]},"
+#  puts "Y: #{colors[2]},"
+#  puts "G: #{colors[3]},"
+#  puts "B: #{colors[4]},"
+#  puts "I: #{colors[5]},"
+#  puts "V: #{colors[6]}"
+
+
+
+#  puts colors[2]
+#  puts colors[3]
+#  puts colors[4]
+#  puts colors[5]
+#  puts colors[6]
+#  puts colors[7]

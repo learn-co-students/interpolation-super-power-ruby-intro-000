48ec13f1230c14389cba003dc1293bab441ec7b9
John Sillings <johnsillings@gmail.com>
2018-02-10 04:06:50 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(cheebler)
+  puts "R: #{cheebler[0]}, O: #{cheebler[1]}, Y: #{cheebler[2]}, G: #{cheebler[3]}, B: #{cheebler[4]}, I: #{cheebler[5]}, V: #{cheebler[6]}"
+end

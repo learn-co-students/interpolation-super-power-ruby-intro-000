6c3348cbc89ae930063dd074d55875db6d8e2333
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 00:22:47 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors=[])
+  colors.collect! do |color|
+    "#{color[0].upcase}: #{color}"
+  end
+  puts colors.join(', ')
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])

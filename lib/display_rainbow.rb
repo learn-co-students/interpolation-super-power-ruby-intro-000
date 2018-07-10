460f42f77856647ee026dc4703c0c647a1b00721
Brian Davis <turbohappy@gmail.com>
2018-07-10 22:22:11 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  blah = []
+  for i in 0..colors.size-1
+    blah.push(exp_color(colors[i]))
+  end
+  # blah = colors.map { |color| exp_color(color) }
+  puts blah.join(", ")
+end
+
+def exp_color(color)
+  color.slice(0,1).upcase+': '+color
+end
+#
+# display_rainbow ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']

061e6ba5d1d7d458567f4cc52ad3fdab273ae43f
Kyung Won Park <stomg7969@gmail.com>
2019-01-27 00:28:41 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(arg)
+  puts "#{arg[0][0].upcase}: #{arg[0]}, #{arg[1][0].upcase}: #{arg[1]}, #{arg[2][0].upcase}: #{arg[2]}, #{arg[3][0].upcase}: #{arg[3]}, #{arg[4][0].upcase}: #{arg[4]}, #{arg[5][0].upcase}: #{arg[5]}, #{arg[6][0].upcase}: #{arg[6]}"
+end

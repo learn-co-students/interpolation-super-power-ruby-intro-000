00b904b4d32fee2cd4c8777d7cd130500f2e5edb
avid-terminal-4100 <shinelior@gmail.com>
2019-04-12 08:14:59 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+color=["R:red","O:orang","Y:yellow","G:green","B:blue","I:indigo","V:violet"]
+def display_rainbow(x)
+  puts " rainbow:#{color[0]},o:#{color[1]},y:#{color[2]},G:#{color[3]},B:#{color[4]},I:#{color[5]},v:#{color[6]}"
+end
+display_rainbow(color)

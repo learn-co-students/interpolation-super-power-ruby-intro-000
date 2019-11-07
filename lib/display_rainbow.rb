c9deb089df7fe726ac84d413a4911c0484a203fc
lior Shine <shinelior@gmail.com>
2019-11-07 23:42:08 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,18 +1,40 @@
 def display_rainbow(colors)
   
- colors=["red","orange","yellow","green","blue","indigo","violet"]
- letters=["R:","O:","Y:","G:","B:","I:","V:"]
-  
-  print letters.zip(colors)
-  
+print colors
 end
-colors=["red,","orange,","yellow,","green,","blue,","indigo,","violet,"]
-colors= colors[0]
+colors1=["red,","orange,","yellow,","green,","blue,","indigo,","violet,"]
+ letters=["R","O","Y","G","B","I","V"]
+ colors2=["","","","","","","","",""]
+ colors2 =letters.zip(colors1)
+  
+
+colors=colors2[0]
 display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors2[1]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors2[2]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+colors=colors2[3]
 
 
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
 
+colors=colors2[4]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
 
+colors=colors2[5]
+display_rainbow(colors)
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+colors=colors2[6]
+display_rainbow(colors)
 
 
   

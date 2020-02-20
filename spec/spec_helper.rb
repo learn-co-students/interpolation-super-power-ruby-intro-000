c9440449e0b0b6750e0565dd46dd56efed95c253
Xi Chen <stephaniechen319@gmail.com>
2020-02-20 19:21:58 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,11 @@ require_relative "../lib/display_rainbow.rb"
 RSpec.configure do |config|
   RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 end
+
+
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end

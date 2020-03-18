9d5d540666d44dd1313baeafd59885a2a0e054e9
Maysoon Tabali <mtab03@me.com>
2020-03-18 05:11:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,9 @@
 #def display_rainbow(colors)
   #puts colors (", ")
 #end
+#def display_rainbow(colors)
+#  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+#end
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end

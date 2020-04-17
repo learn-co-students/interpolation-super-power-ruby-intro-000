1994c11f31daf827fa46720a9a8161a162a6b4c4
Avni Alp Mumcu <a.alp.mumcu@gmail.com>
2020-04-17 19:42:52 +0000

    Done.

---
+++ mumcus.rb
@@ -0,0 +1,13 @@
+aydin = ["Aydin", "1", "20/09/2020", "Being a baby!"]
+kristine = ["Kristine", "42", "29/04/1978", "Paraprofessional!"]
+avni = ["Avni", "39", "09/06/1981", "Programmer!"]
+
+def mumcus(family)
+
+  puts "Name: #{family[0]}, Age: #{family[1]}, Birthday: #{family[2]}, Occupation: #{family[3]}"
+
+end
+
+mumcus(aydin)
+mumcus(kristine)
+mumcus(avni)

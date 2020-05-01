eaef0337967dde7ca075950a4acbaca537179586
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-01 01:10:19 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,4 @@ puts "The fourth color in the rainbow is: #{colors[3]}"
 puts "The fifth color in the rainbow is: #{colors[4]}"
 puts "The sixth color in the rainbow is: #{colors[5]}"
 puts "The seventh color in the rainbow is: #{colors[6]}"
-def display_rainbow
-  puts "Color: #{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}"
-end
 print display_rainbow

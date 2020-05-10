6152202545c2810c80b036aa7433802855ccbc3b
Ermite Rikudo <opouth@hotmail.fr>
2020-05-10 04:05:25 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,9 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(colors)
   puts "the colors of the rainbow is #{colors}"
 end
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+display_rainbow(colors)
 
-display_rainbow(["#{colors[0][0].capitalize}: #{colors[0]}, #{colors[1][0].capitalize}: #{colors[1]}, #{colors[2][0].capitalize}: #{colors[2]}, #{colors[3][0].capitalize}: #{colors[3]}, #{colors[4][0].capitalize}: #{colors[4]}, #{colors[5][0].capitalize}: #{colors[5]}, #{colors[6][0].capitalize}: #{colors[6]}"
-]) 
+def display_rainbow(colors)
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+display_rainbow(colors)

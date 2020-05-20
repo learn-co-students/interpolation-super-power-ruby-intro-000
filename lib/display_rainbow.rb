fa1e4975d8dadd61d3cec9acf2c8f2a7300a1d51
Mykyta Melnyk <30383@pwsz-kalisz.edu.pl>
2020-05-20 10:00:28 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,20 @@
 # Write your #display_rainbow method here
+
+# def display_rainbow(colors)
+#   $string = ""
+#   i = 0
+#   for color in colors
+#     if i < colors.length() - 1
+#       $string += "#{color[0].upcase}: #{color}, "
+#     else
+#       $string += "#{color[0].upcase}: #{color}"
+#     end
+#     i += 1
+#   end
+  
+#   puts $string
+# end
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end

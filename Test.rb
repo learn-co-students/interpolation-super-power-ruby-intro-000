a0d9f08641a7b8a21393f3af296e9607f00354e8
Deja Monae Allison <dejaallison@gmail.com>
2020-06-12 00:42:36 +0000

    Done.

---
+++ Test.rb
@@ -0,0 +1,4 @@
+bob     = ["Bob", 46, "Juggler"]
+stefani = ["Stefani", 49, "Firefighter"]
+puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
+puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"

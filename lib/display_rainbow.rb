c8fe4f6fc8365ae5d07138d04e8bee8fffa236c9
Will Vetter <vetter.william@gmail.com>
2020-07-30 08:17:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(array_of_colors)
+  print "#{array_of_colors[0][0].capitalize}: #{array_of_colors[0]}, "
+  print "#{array_of_colors[1][0].capitalize}: #{array_of_colors[1]}, "
+  print "#{array_of_colors[2][0].capitalize}: #{array_of_colors[2]}, "
+  print "#{array_of_colors[3][0].capitalize}: #{array_of_colors[3]}, "
+  print "#{array_of_colors[4][0].capitalize}: #{array_of_colors[4]}, "
+  print "#{array_of_colors[5][0].capitalize}: #{array_of_colors[5]}, "
+  puts "#{array_of_colors[6][0].capitalize}: #{array_of_colors[6]}"
+
+end

bc4a182c43acabeba1c94b57da237909a6925b72
William Stoneberg <wrstoneberg@me.com>
2020-09-14 00:01:29 +0000

    Done.

---
+++ display_rainbow.rb
@@ -0,0 +1,5 @@
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "viloet"]
+def display_rainbow(colors)
+  puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]},
+        G: #{display_rainbow[3]}, B: #{display_rainbow[4]},I: #{display_rainbow[5]}, V: #{display_rainbow[6]}"
+end

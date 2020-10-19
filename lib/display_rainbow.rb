95c13593a3750a24cff2d790a54affa195168c5a
James Shreckengost <shreckthegent@gmail.com>
2020-10-19 13:57:18 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,25 @@
 # Write your #display_rainbow method here
+#display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+#letters = ["b","a","c"]
+#puts "The first letter in the alphabet is: #{letters[1]}"
+#puts "The second letter in the alphabet is: #{letters[0]}"
+#puts "The third letter in the alphabet is: #{letters[2]}"
+
+#My Wrong Code
+#colors = {"red","orange","yellow","green","blue","indigo","violet"}
+#letters = ["R", "O", "Y", "G", "B", "I","V"]
+#"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+
+#puts "The first letter in the alphabet is #{letters[0]}"
+#puts "The second letter in the alphabet is #{letters[1]}"
+#puts "The third letter in the alphabet is #{letters[2]}"
+#puts "The fourth letter in the alphabet is #{letters[3]}"
+#puts "The fifth letter in the alphabet is #{letters[4]}"
+#puts "The sixth letter in the alphabet is #{letters[5]}"
+#puts "The seventh letter in the alphabet is #{letters[6]}"
+
+
+  def display_rainbow(colors)
+   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+   
+end

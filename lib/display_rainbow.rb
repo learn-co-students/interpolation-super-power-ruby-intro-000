2f91e0e43b58a66702ade4492fbd2fc06115491f
remarkable-game-d37c <georgejermine@gmail.com>
2021-02-09 15:59:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
-# Write your #display_rainbow method here
+colors=['red','orange','yellow','green','blue','indigo','violet']
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end

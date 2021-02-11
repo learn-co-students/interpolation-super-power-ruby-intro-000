8f0aeb0bf28449cd181b53d055bfb6724d61f564
Geoffrey Ontiri <ontirischool@gmail.com>
2021-02-11 01:32:37 +0000

    Done.

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,3 +1,6 @@
+def display_rainbow (colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
 describe '#display_rainbow' do
   it 'accepts one argument' do
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']

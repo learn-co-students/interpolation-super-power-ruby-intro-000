be25dc27337e5c15ff39ad3d1b1d5f8f85ea544a
Morgan Keith <morgan.a.keith92@gmail.com>
2021-04-08 01:42:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+#def print_business_card(person)
+##  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+#  puts "Contact #{person[3]}"
+#end
+def display_rainbow(color)
+  puts "R: #{color[0]} O: #{color[1]} Y: #{color[2]} G: #{color[3]} B: #{color[4]} I: #{color[5]} V: #{color[6]} "
+end

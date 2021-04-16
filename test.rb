50989d5da762330d0935ae5a007302ede9a92319
Martin Ogden <martin.ogden62@gmail.com>
2021-04-16 17:53:08 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,12 @@
+
+bob = ["Bob", 46, "Juggler", "555-555-5555"]
+stefani = [ "Stefani", 49, "Firefighter", "555-111-1111"]
+
+def print_business_card(person)
+puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+puts "Contact: #{person[3]}"
+end
+
+print_business_card(bob)
+
+print_business_card(stefani)
